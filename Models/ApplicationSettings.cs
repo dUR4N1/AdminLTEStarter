18903aa63ca02808a089c3311eaafa415e024904
imBdurrani <durrani.gpe@gmail.com>
2020-12-06 19:08:32 +0500

    Add project files.

---
+++ Models/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTEStarter.Models
+{
+    public class ApplicationSettings
+    {
+        public bool AutoConfirmEmail { get; set; }
+    }
+}
